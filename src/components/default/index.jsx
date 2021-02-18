f3e8f9662409fb1a5a27525460d84c7e5de55837
kanekotic <me@kanekotic.com>
2021-02-18 18:02:07 +0100

    Alvaro | add pouchdb

--- src/components/default/index.jsx
+++ src/components/default/index.jsx
@@ -1,9 +1,16 @@
 import * as React from "react"
 import { useEffect } from "react"
 import "./index.scss"
+import PouchDB from 'pouchdb';
 
 const DefaultPage = ({ finishedLoading }) => {
   useEffect(finishedLoading ,[finishedLoading]);
+  useEffect(() => {
+    var db = new PouchDB('askkyn');
+    db.info().then(function (info) {
+      console.log(info);
+    })    
+  },[])
 
   return (<>
     Welcome to Askkyn
