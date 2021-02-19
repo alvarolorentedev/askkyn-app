dd3c2ffaa7e095dac5a190838a3ba58ef992a90b
kanekotic <me@kanekotic.com>
2021-02-19 17:49:16 +0100

    Alvaro | connect to correct backend

--- src/service/index.js
+++ src/service/index.js
@@ -2,6 +2,8 @@ import PouchDB from 'pouchdb'
 import { create } from './game/create'
 import { join } from './game/join'
 
-export const db = new PouchDB('http://localhost:8080/db/askkyn');
+const backendBaseUrl = process.env.NODE_ENV === 'production' ? "https://askkyn.herokuapp.com" : "http://localhost:8080"
+
+export const db = new PouchDB(`${backendBaseUrl}/db/askkyn`);
 export const createGame = () => create(db)
 export const joinGame = (identifier, username) => join(db, identifier, username)
